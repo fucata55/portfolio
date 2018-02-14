eb41c732e9599f1de6a871d0d6da4a2f1238be9a
fucata <sliong325@gmail.com>
2018-02-14 03:01:25 -0600

    Added CSS details and scrolling feature

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-$clamp(paragaph, {
-    clamp: '35px'
-})
+$('.scroll-about').click(function () {
+    $('html,body').animate({
+        scrollTop: $('#aboutMe').offset().top
+    }, 'slow');
+});
+
+$('.scroll-projects').click(function () {
+    $('html,body').animate({
+        scrollTop: $('#projects').offset().top
+    }, 'slow');
+});
+
+$('.scroll-skills').click(function () {
+    $('html,body').animate({
+        scrollTop: $('#skills').offset().top
+    }, 'slow');
+});
+
+$('.scroll-recommendations').click(function () {
+    $('html,body').animate({
+        scrollTop: $('#recommendations').offset().top
+    }, 'slow');
+});
+
+$('.scroll-contact').click(function () {
+    $('html,body').animate({
+        scrollTop: $('footer').offset().top
+    }, 'slow');
+});
