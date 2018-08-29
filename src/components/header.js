21c88f9edfe131d531dffde7fd71ed8131dc2c08
fucata <sliong325@gmail.com>
2018-08-28 21:24:06 -0500

    Update html tags to JSX

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import stephenIcon from "../../public/static/images/icons/Stephen-icon.png"
 import accountIcon from "../../public/static/images/icons/account-icon.png"
 import projectsIcon from "../../public/static/images/icons/projects-icon.png"
@@ -8,19 +7,19 @@ import skillsIcon from "../../public/static/images/icons/skills-icon.png"
 const Header = () => (
   <header role="banner">
     <nav role="navigation">
-      <li class="nav-item" id="heroSectionLink">
+      <li className="nav-item" id="heroSectionLink">
         <img src={stephenIcon} alt="Stephen's icon" />
         Stephen
       </li>
-      <li class="nav-item" id="aboutSectionLink">
+      <li className="nav-item" id="aboutSectionLink">
         <img src={accountIcon} alt="About icon" />
         About
       </li>
-      <li class="nav-item" id="projectSectionLink">
+      <li className="nav-item" id="projectSectionLink">
         <img src={projectsIcon} alt="Projects icon" />
         Projects
       </li>
-      <li class="nav-item" id="skillSectionLink">
+      <li className="nav-item" id="skillSectionLink">
         <img src={skillsIcon} alt="Skills icon" />
         Skills
       </li>
