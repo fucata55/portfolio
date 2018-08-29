d53c9ca2a8ed3aa3de04095b7e4a2957c3259eb1
fucata <sliong325@gmail.com>
2018-08-29 07:58:46 -0500

    exported footer

---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer role="complementary">
+        <div>
+            <a className="contact-link" href="https://github.com/fucata55" target="_blank" role="link">
+                <img src="images/icons/GitHub-icon.png" alt="Link to Stephen's GitHub" role="img" />
+            </a>
+            <a className="contact-link" href="https://www.linkedin.com/in/stephen-liong-75252775/" target="_blank" role="link">
+                <img src="images/icons/LinkedIn-icon.png" alt="Link to  Stephen's LinkedIn" role="img" />
+            </a>
+            <a className="contact-link" href="mailto:sliong325@gmail.com" target="_blank" role="link">
+                <img src="images/icons/Gmail-icon.png" alt="Link to Stephen's Gmail" role="img" />
+            </a>
+            <a className="contact-link" href="https://twitter.com/sfucata" target="_blank" role="link">
+                <img src="images/icons/Twitter-icon.png" alt="Link to Stephen's Tweeter" role="img" />
+            </a>
+        </div>
+        <p>Photography by <a href="https://annajoyblog.wordpress.com/" target="_blank">Anna Sandmeier</a></p>
+        <p>WaterColour splatter <a href="https://www.freepik.com/free-vector/watercolour-splatter-in-rainbow-colours_1215454.html">designed by Freepik</a></p>
+    </footer>
+)
+
+export default Footer
