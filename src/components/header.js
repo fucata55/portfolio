77cc7316e0ce37ee467fcf42d3a026c5dbba7121
fucata <sliong325@gmail.com>
2018-08-28 20:55:00 -0500

    Added Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import stephenIcon from "../../public/static/images/icons/Stephen-icon.png"
+import accountIcon from "../../public/static/images/icons/account-icon.png"
+import projectsIcon from "../../public/static/images/icons/projects-icon.png"
+import skillsIcon from "../../public/static/images/icons/skills-icon.png"
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const Header = () => (
+  <header role="banner">
+    <nav role="navigation">
+      <li class="nav-item" id="heroSectionLink">
+        <img src={stephenIcon} alt="Stephen's icon" />
+        Stephen
+      </li>
+      <li class="nav-item" id="aboutSectionLink">
+        <img src={accountIcon} alt="About icon" />
+        About
+      </li>
+      <li class="nav-item" id="projectSectionLink">
+        <img src={projectsIcon} alt="Projects icon" />
+        Projects
+      </li>
+      <li class="nav-item" id="skillSectionLink">
+        <img src={skillsIcon} alt="Skills icon" />
+        Skills
+      </li>
+    </nav>
+  </header>
 )
 
 export default Header
