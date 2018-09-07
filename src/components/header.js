601c01494a8f30358b211ee27849fd724561fe0c
fucata <sliong325@gmail.com>
2018-09-06 22:38:53 -0500

    Initiated gatsby Image

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,27 @@
 import React from 'react'
-import stephenIcon from "../../public/static/images/icons/Stephen-icon.png"
-import accountIcon from "../../public/static/images/icons/account-icon.png"
-import projectsIcon from "../../public/static/images/icons/projects-icon.png"
-import skillsIcon from "../../public/static/images/icons/skills-icon.png"
+import Img from "gatsby-image";
 
-const Header = () => (
-  <header role="banner">
-    <nav role="navigation">
-      <li className="nav-item" id="heroSectionLink">
-        <img src={stephenIcon} alt="Stephen's icon" />
-        Stephen
-      </li>
-      <li className="nav-item" id="aboutSectionLink">
-        <img src={accountIcon} alt="About icon" />
-        About
-      </li>
-      <li className="nav-item" id="projectSectionLink">
-        <img src={projectsIcon} alt="Projects icon" />
-        Projects
-      </li>
-      <li className="nav-item" id="skillSectionLink">
-        <img src={skillsIcon} alt="Skills icon" />
-        Skills
-      </li>
-    </nav>
-  </header>
+const Header = props => (
+    <header role="banner">
+        <nav role="navigation">
+          <li className="nav-item" id="heroSectionLink">
+            <Img resolutions={props.images.stephenIcon.childImageSharp.resolutions} alt="Stephen's icon" />
+            Stephen
+          </li>
+          <li className="nav-item" id="aboutSectionLink">
+            <Img resolutions={props.images.accountIcon.childImageSharp.resolutions} alt="About icon" />
+            About
+          </li>
+          <li className="nav-item" id="projectSectionLink">
+            <Img resolutions={props.images.projectsIcon.childImageSharp.resolutions} alt="Projects icon" />
+            Projects
+          </li>
+          <li className="nav-item" id="skillSectionLink">
+            <Img resolutions={props.images.skillsIcon.childImageSharp.resolutions} alt="Skills icon" />
+            Skills
+          </li>
+        </nav>
+      </header>
 )
 
 export default Header
