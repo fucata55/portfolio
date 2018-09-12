685aa40a3e944aa27b6fbd655bca79709c222130
fucata <sliong325@gmail.com>
2018-09-11 19:56:14 -0500

    drying code

--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -19,21 +19,22 @@ const BgImage = styled(Img)`
 
 const SkillSection = props => {
     let bg = props.images.skillBg.childImageSharp.sizes
+    let image = name => props.images[name].childImageSharp.resolutions
     return (
     <section id="skillSection">
-        <h3 role="heading"><Img resolutions={props.images.skillsIcon.childImageSharp.resolutions} alt="Skills icon" />Skills and Tools</h3>
+        <h3 role="heading"><Img resolutions={image("skillsIcon")} alt="Skills icon" />Skills and Tools</h3>
         <div className="main-container">
-            <Figure bg={bg} link="https://www.w3schools.com/html/html_intro.asp" img={props.images.htmlLogo.childImageSharp.resolutions} caption="HTML" />
-            <Figure bg={bg} link="https://www.w3schools.com/css/" img={props.images.cssLogo.childImageSharp.resolutions} caption="CSS" />
-            <Figure bg={bg} link="https://developer.mozilla.org/en-US/docs/Web/JavaScript" img={props.images.jsLogo.childImageSharp.resolutions} caption="JavaScript" />
-            <Figure bg={bg} link="https://jquery.com/" img={props.images.jQueryLogo.childImageSharp.resolutions} caption="jQuery" />
-            <Figure bg={bg} link="https://reactjs.org/" img={props.images.reactLogo.childImageSharp.resolutions} caption="React JS" />
-            <Figure bg={bg} link="https://nodejs.org/en/about/" img={props.images.nodeLogo.childImageSharp.resolutions} caption="Node JS" />
-            <Figure bg={bg} link="https://www.mongodb.com/" img={props.images.mongoDbLogo.childImageSharp.resolutions} caption="MongoDb" />
-            <Figure bg={bg} link="https://kotlinlang.org/" img={props.images.kotlinLogo.childImageSharp.resolutions} caption="Kotlin" />
-            <Figure bg={bg} link="https://www.jetbrains.com/idea/" img={props.images.intelliJLogo.childImageSharp.resolutions} caption="intelliJ" />
-            <Figure bg={bg} link="https://tornadofx.io/" img={props.images.tornadoFXLogo.childImageSharp.resolutions} caption="Tornado FX" />
-            <Figure bg={bg} link="https://github.com/fucata55" img={props.images.gitHubLogo.childImageSharp.resolutions} caption="GitHub" />
+            <Figure bg={bg} link="https://www.w3schools.com/html/html_intro.asp" img={image("htmlLogo")} caption="HTML" />
+            <Figure bg={bg} link="https://www.w3schools.com/css/" img={image("cssLogo")} caption="CSS" />
+            <Figure bg={bg} link="https://developer.mozilla.org/en-US/docs/Web/JavaScript" img={image("jsLogo")} caption="JavaScript" />
+            <Figure bg={bg} link="https://jquery.com/" img={image("jQueryLogo")} caption="jQuery" />
+            <Figure bg={bg} link="https://reactjs.org/" img={image("reactLogo")} caption="React JS" />
+            <Figure bg={bg} link="https://nodejs.org/en/about/" img={image("nodeLogo")} caption="Node JS" />
+            <Figure bg={bg} link="https://www.mongodb.com/" img={image("mongoDbLogo")} caption="MongoDb" />
+            <Figure bg={bg} link="https://kotlinlang.org/" img={image("kotlinLogo")} caption="Kotlin" />
+            <Figure bg={bg} link="https://www.jetbrains.com/idea/" img={image("intelliJLogo")} caption="intelliJ" />
+            <Figure bg={bg} link="https://tornadofx.io/" img={image("tornadoFXLogo")} caption="Tornado FX" />
+            <Figure bg={bg} link="https://github.com/fucata55" img={image("gitHubLogo")} caption="GitHub" />
         </div>
     </section>
 )}
